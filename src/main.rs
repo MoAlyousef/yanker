856e6012141c4442a5e1a1c025976003c1a017b0
MoAlyousef <may642_2000@hotmail.com>
2021-03-08 02:07:43 +0300

    add comments

--- src/main.rs
+++ src/main.rs
@@ -16,36 +16,7 @@
 //! Should yank all versions between 0.1.1 (included) to 0.2.5 (excluded).
 //!
 
-const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"));
-
-lazy_static::lazy_static! {
-    static ref RE: regex::Regex = regex::Regex::new(r"\[(.*), *(.*)\]").unwrap();
-}
-
-#[derive(Debug, serde::Deserialize)]
-struct Package {
-    name: String,
-}
-
-#[derive(Debug, serde::Deserialize)]
-struct Config {
-    package: Package,
-}
-
-#[derive(Debug, serde::Deserialize)]
-struct Version {
-    #[serde(rename = "crate")]
-    crate_name: String,
-    #[serde(rename = "num")]
-    version: String,
-    yanked: bool,
-}
-
-#[derive(Debug, serde::Deserialize)]
-struct Versions {
-    versions: Vec<Version>,
-}
-
+// help message
 const HELP: &str = r#"
 Yank a range of crate versions
 
@@ -58,16 +29,51 @@ OPTIONS:
     --help                   Prints help information
 "#;
 
+// required by crates.io
+const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"));
+
+// regex for cli arg
+lazy_static::lazy_static! {
+    static ref RE: regex::Regex = regex::Regex::new(r"\[(.*), *(.*)\]").unwrap();
+}
+
+// Cargo.toml package.name
+#[derive(Debug, serde::Deserialize)]
+struct Package {
+    name: String,
+}
+
+// Cargo.toml package
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    package: Package,
+}
+
+// json crate, num, yanked
+#[derive(Debug, serde::Deserialize)]
+struct Version {
+    #[serde(rename = "crate")]
+    crate_name: String,
+    #[serde(rename = "num")]
+    version: String,
+    yanked: bool,
+}
+
+// json versions
+#[derive(Debug, serde::Deserialize)]
+struct Versions {
+    versions: Vec<Version>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // get command line args
     let mut args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         println!("{}", HELP);
         return Ok(());
     }
-
     let main_arg = args.pop().ok_or("")?;
-
     match main_arg.as_str() {
         "--help" | "-h" => {
             println!("{}", HELP);
@@ -80,6 +86,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         _ => (),
     }
 
+    // get minimimun(included) and maximum(excluded) crate versions
     let (from, to) = {
         if RE.is_match(&main_arg) {
             let caps = RE.captures(&main_arg).ok_or("")?;
@@ -91,12 +98,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
+    // read Cargo.toml and deserialize
     let local_toml = std::fs::read_to_string("Cargo.toml")?;
     let local_crate: Config = toml::from_str(&local_toml)?;
 
+    // construct client
     let client = reqwest::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()?;
+    // launch request and deserialize json
     let resp = client
         .get(&format!(
             "https://crates.io/api/v1/crates/{}/versions",
@@ -107,7 +117,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json::<Versions>()
         .await?;
 
-    let v: Vec<String> = resp
+    // put matching versions in a Vec
+    let versions: Vec<String> = resp
         .versions
         .iter()
         .filter(|item| {
@@ -121,9 +132,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map(|item| item.version.clone())
         .collect();
 
-    for elem in v {
+    // iterate versions and yank
+    for version in versions {
         tokio::process::Command::new("cargo")
-            .args(&["yank", "--vers", &elem])
+            .args(&["yank", "--vers", &version])
             .spawn()?
             .wait()
             .await?;
